7873df3a12387b13092c8cb917f2fa206ae771a1
Rodi420 <rodrigo.viamontes@outlook.com>
2024-11-21 16:23:03 +0100

    updated main to sync settings properly

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 import 'screens/dashboard.dart';
+import 'package:provider/provider.dart';
+import 'providers/settings_provider.dart';
+import 'screens/settings_screen.dart';
+import 'themes/theme_builder.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => SettingsProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TrackG',
-      theme: ThemeData.dark(), // Dunkles Thema für den Stil
-      home: DashboardScreen(), // Hier die neue Seite einfügen
+    return Consumer<SettingsProvider>(
+      builder: (context, settingsProvider, child) {
+        return MaterialApp(
+          title: 'TrackG',
+          theme: ThemeBuilder.getTheme(settingsProvider.currentTheme),
+          darkTheme: ThemeBuilder.getTheme('Dark'),
+          themeMode: settingsProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          home: DashboardScreen(),
+        );
+      },
     );
   }
 }
